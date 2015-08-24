409718ce889d810d2f82150edd464febfefe6a8b
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-08-25 02:37:24 +0530

    Value can be populated when dom loaded

--- jqtags.select.js
+++ jqtags.select.js
@@ -1,11 +1,17 @@
 _tag_("jqtags.select",function(select){
 	
 	var jq = module("jQuery");
+  var makeOptionString = function(option){
+    return "<option value='" + option.id + "'>" + option.text + "</option>";
+  };
+  var makeOptionGroup = function(option){
+    return "<optgroup label='"+option.text+"'>";
+  };
 	
 	return {
 	    tagName: "jq-select",
 	    events: {
-	    	"changed.bs.select" : "updateOptions",
+	    	"changed.bs.select" : "updateOptions"
 	    	//"change" : "oMyChange",
 	    	//"input" : "oMyChange"
 	    },
@@ -33,16 +39,33 @@ _tag_("jqtags.select",function(select){
 		    		+ (this.$.multiple?"multiple" : "") +
 		    		">"+this.$.innerHTML+"</select>";
 		    	this.$select = jq(this.$).find("select");
-		    	this.$select.val((this.$.value+"").split(","))
+		    	this.$select.val((this.$.value+"").split(","));
 		    	this.$select.data(this.$.dataset).selectpicker();
 		    	this.$select.detach();
 		    	this.$select.change(function(e){
-		    		console.log("changing",e);
 		    		self.$.value = self.$select.val();
-		    		//self.$.multivalue = self.$select.val();
 		    		self.trigger("change");
 		    		self.trigger("input");
 		    	});
+          this.trigger("jq.query", {
+            value : this.$.value,
+            callback : function(data){
+              var optionsString = "";
+              for(var i in data){
+                if(data[i].children!==undefined){
+                  optionsString+=makeOptionGroup(data[i]);
+                  for(var j in data[i].children){
+                    optionsString+=makeOptionString(data[i].children[j]);
+                  }
+                  optionsString+="</option>";
+                } else {
+                  optionsString+=makeOptionString(data[i]);
+                }
+              }
+              self.$select.html(optionsString);
+              self.$select.selectpicker("refresh");
+            }
+          });
 	    	} else {
 	    		jq(this.$).selectpicker();
 	    	}
